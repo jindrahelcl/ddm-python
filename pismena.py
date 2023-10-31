20477ae006ee7a826c8af9195f04ccd1df833423
jindra <jindra.helcl@gmail.com>
2023-10-31 18:34:09 +0100

    hgfddfgh

---
+++ pismena.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+def pismeno_a():
+    forward(50)
+    # tady nakreslit zbytek
+    penup() # ted to nekreslí
+    forward(50)  # posunu se o 50 a nekreslim
+    pendown()  # ted zase zacnu kreslit;
+
+
+pismeno_a()
+exitonclick()

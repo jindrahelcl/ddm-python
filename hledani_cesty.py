802bae43001a531f8ae1d3f7c8e12a0ebf2252a6
jindra <jindra.helcl@gmail.com>
2024-03-12 18:02:27 +0100

    hodina 12.3.

---
+++ hledani_cesty.py
@@ -0,0 +1,37 @@
+
+mistnosti = ["0-obývák", "1-chodba", "2-sklep", "3-trůnní sál","4-jídelna","5-terasa","6-půda","7-kumbál","8-sídlo","9-ložnice","10-chodba","11-tajemna komnata"]
+chodby = [[1, 2,4], [0,4,6], [0,11], [1, 2],[1,0,5,7],[4,6,10],[1,5],[4],[10],[10],[9,8,5],[2]]
+
+start = 0
+konec = 5
+
+pozice = start
+cesty = [[start]]
+
+
+while True:
+    cesta = cesty.
+
+
+
+#
+# while pozice != konec:
+#     moznosti = chodby[pozice]
+#
+#     for m in moznosti:
+#         if m not in navstiveno:
+#             # ted tam muzu jit.
+#             pozice = m
+#             break
+#
+#     print("jdu do ", mistnosti[pozice])
+#
+
+
+
+
+
+
+
+
+

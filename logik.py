628198618fb71a0bb1a012c0829980c61dfa2c13
jindra <jindra.helcl@gmail.com>
2024-04-23 18:27:30 +0200

    logik - základ

---
+++ logik.py
@@ -0,0 +1,44 @@
+import random
+
+POZIC = 5
+BAREV = 8
+
+zadani = []
+for i in range(POZIC):
+    zadani.append(random.randint(0, BAREV - 1))
+print(zadani)
+
+hotovo = False
+while not hotovo:
+    tip_retezec = input("Dodej tip: ")
+    tip = []
+    for d in tip_retezec:
+        tip.append(int(d))
+    if len(tip) != POZIC:
+        print("spatne")
+        continue
+
+    cerny = 0
+    for i in range(POZIC):
+        if tip[i] == zadani[i]:
+            cerny += 1
+
+    bily = 0
+    for i in range(BAREV):
+        v_tipu = 0
+        v_zadani = 0
+        for j in range(POZIC):
+            if tip[j] == zadani[j]:
+                continue
+
+            if tip[j] == i:
+                v_tipu += 1
+            if zadani[j] == i:
+                v_zadani += 1
+
+        bily += min(v_tipu, v_zadani)
+
+    print(cerny, bily)
+
+    if cerny == POZIC:
+        hotovo = True

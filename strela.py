00197ed1948e25a4bad84f72ee17110311b44af1
jindra <jindra.helcl@gmail.com>
2024-05-14 17:45:02 +0200

    update

---
+++ strela.py
@@ -0,0 +1,14 @@
+
+class Strela:
+    def __init__(self, pozice, smer):
+        self.pozice = pozice
+        self.smer = smer
+
+    def nakresli(self, screen):
+        pass
+
+    def posun(self):
+        # kod kterej zmeni pozici o smer kazdy frame
+        pass
+
+s = Strela([0, 50], 100)
